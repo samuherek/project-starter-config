f7b0fbb41fe205ad775953fcc6eff13e4f0f9658
Sam Uherek <samuherekbiz@gmail.com>
2019-01-20 20:25:14 +0100

    add SVGR config with Typescript

---
+++ .svgrrc.js
@@ -0,0 +1,24 @@
+// Typescript
+module.exports = {
+  ext: 'tsx',
+  icon: true,
+  svgProps: {
+    'aria-hidden': 'true',
+    display: 'block',
+    fill: 'currentColor',
+    focusable: 'false',
+    role: 'presentation',
+  },
+  template(
+    { template },
+    opts,
+    { imports, componentName, props, jsx, exports }
+  ) {
+    const typeScriptTpl = template.smart({ plugins: ['typescript'] });
+    return typeScriptTpl.ast`
+    import * as React from 'react';
+    const ${componentName} = (props: React.SVGProps<SVGSVGElement>) => ${jsx};
+    export default ${componentName};
+  `;
+  },
+};
